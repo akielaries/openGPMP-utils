f88e3b70cfbf0562ed4b685ee1150ba9ed496ce7
montaquila <akiel@akiel.org>
2023-02-05 00:24:51 -0700

    adding test.sh script with idea

---
+++ test.sh
@@ -0,0 +1,6 @@
+# I am hoping to create some shell script based tooling for 
+# creating some easy integration between this repository 
+# that houses datasets, and openMTPK that makes use of them 
+# for testing/validation
+
+
